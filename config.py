c3057a8a9a92e0b44a055fa28c97a876d19ace0e
Verifyhsjajjsjsj <mykakjsjjsos@gmail.com>
2025-01-30 17:30:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config(object):
     API_HASH = os.environ.get("ba25181c01b50d945748801b6c8b6ecc")
     BOT_TOKEN = os.environ.get("7806319439:AAG_5sVnQGQdb6E4GKHxoLG5DLljEgwSfxE")
     TELEGRAM_API = os.environ.get("26162072")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    OWNER = os.environ.get("6717382350")
+    OWNER_USERNAME = os.environ.get("Stylish_Star2")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("-1002252886633")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
